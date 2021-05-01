d13c4139fdd813836c2c5b51c1de7dae98467c6d
Dominic Widdows <dwiddows@liveperson.com>
2021-05-01 12:49:17 -0700

    Added country names

--- map_sea.py
+++ map_sea.py
@@ -2,7 +2,9 @@
 
 Much of this could be factored out into more general utility functions and interfaces.
 """
-from PIL import Image, ImageDraw
+import math
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 import shapefile
 
 BORDER = 20
@@ -13,19 +15,36 @@ ALL_COUNTRY_RECORDS = shapefile.Reader("./data/ne_50m_admin_0_countries.shp")
 Change to a different set of countries and near countries to get a different map."""
 SEA_COUNTRIES = ['Indonesia', 'Philippines', 'Vietnam', 'Thailand', 'Myanmar', 'Malaysia', 'Singapore', 'Cambodia',
                  'Laos', 'Brunei', 'East Timor']
-NEAR_COUNTRIES = ["People's Republic of China", 'India', 'Bangladesh', 'Taiwan', 'Australia', 'Papua New Guinea', 'Bhutan']
+NAME_OFFSETS = {
+    "Myanmar": (-10, -50),
+    "Thailand": (0, -50),
+    "Laos": (-30, -40),
+    "Vietnam": (83, 30),
+    "Singapore": (45, 15),
+    "Brunei": (-22, -12),
+    "East Timor": (40, 16),
+    "Cambodia": (0, -8)
+}
+
+NEAR_COUNTRIES = ["People's Republic of China", 'India', 'Bangladesh',
+                  'Taiwan', 'Australia', 'Papua New Guinea', 'Bhutan']
 
 # Routine for setting up overall bounding box, using the excuse that these are literally "global" variables.
 polygons = []
-for shape in ALL_COUNTRY_RECORDS.shapeRecords():
-    if shape.record['NAME_EN'] in SEA_COUNTRIES:
-        polygons.append(shape.shape.points)
+for tmp_shape in ALL_COUNTRY_RECORDS.shapeRecords():
+    if tmp_shape.record['NAME_EN'] in SEA_COUNTRIES:
+        polygons.append(tmp_shape.shape.points)
 
 all_points = [point for polygon in polygons for point in polygon]
 MAXLAT = max([x[0] for x in all_points])
 MINLAT = min([x[0] for x in all_points])
 MAXLON = max([x[1] for x in all_points])
 MINLON = min([x[1] for x in all_points])
+
+
+def point_to_coords(lat_lon_point):
+    return (BORDER + PIXELS_PER_DEGREE * (lat_lon_point[0] - MINLAT),
+            BORDER + PIXELS_PER_DEGREE * (MAXLON - lat_lon_point[1]))
 
 
 def record_to_coords(record):
@@ -36,26 +55,30 @@ def record_to_coords(record):
         start = parts[i]
         end = parts[i + 1] if i + 1 < len(parts) else -1
         polygon = points[start:end]
-        coords = [(BORDER + PIXELS_PER_DEGREE * (x[0] - MINLAT),
-                   BORDER + PIXELS_PER_DEGREE * (MAXLON - x[1])) for x in polygon]
+        coords = [point_to_coords(point) for point in polygon]
         all_coords.append(coords)
     return all_coords
 
 
-def draw_sea_countries(img):
-    for record in ALL_COUNTRY_RECORDS.shapeRecords():
-        if record.record['NAME_EN'] not in SEA_COUNTRIES:
-            continue
+def draw_countries(img_draw, shape_records, fill="#dddddd"):
+    for record in shape_records:
         for coords in record_to_coords(record):
-            img.polygon(coords, fill="#dddddd", outline="grey")
+            img_draw.polygon(coords, fill=fill, outline="grey")
 
 
-def draw_near_countries(img):
-    for record in ALL_COUNTRY_RECORDS.shapeRecords():
-        if record.record['NAME_EN'] not in NEAR_COUNTRIES:
-            continue
-        for coords in record_to_coords(record):
-            img.polygon(coords, fill="#eeeeee", outline="grey")
+def draw_name(img_draw, record):
+    font_type = "Times.ttc"
+    bbox = record.shape.bbox
+    size = bbox[2] - bbox[0] + bbox[3] - bbox[1]
+    font_size = max(math.ceil(2 + math.pow(size, 0.2) * 10), 16)
+    font = ImageFont.truetype(font_type, font_size)
+    name = record.record.NAME_EN
+    w, h = font.getsize(name)
+    center = point_to_coords([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2])
+    top_left = (center[0] - w/2, center[1] - h/2)
+    if name in NAME_OFFSETS:
+        top_left = np.add(top_left, NAME_OFFSETS[name])
+    img_draw.text(top_left, name, font=font, align="center", fill="#333333")
 
 
 def main():
@@ -63,9 +86,19 @@ def main():
             BORDER * 2 + int(PIXELS_PER_DEGREE * (MAXLON - MINLON))]
     img = Image.new("RGB", size, "#f9f9f9")
     img_draw = ImageDraw.Draw(img)
-    draw_sea_countries(img_draw)
-    draw_near_countries(img_draw)
+
+    core_shapes = [record for record in ALL_COUNTRY_RECORDS.shapeRecords() if record.record['NAME_EN'] in SEA_COUNTRIES]
+    near_shapes = [record for record in ALL_COUNTRY_RECORDS.shapeRecords() if
+                   record.record['NAME_EN'] in NEAR_COUNTRIES]
+
+    draw_countries(img_draw, core_shapes)
+    draw_countries(img_draw, near_shapes, fill="#eeeeee")
+
+    for core_shape in core_shapes:
+        draw_name(img_draw, core_shape)
+
     img.show()
+    img.save("sea_countries.png")
 
 
 if __name__ == '__main__':
