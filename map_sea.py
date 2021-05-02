d1bcb8e3d5b611cdb05509fc63a2c2bdde2302f2
Dominic Widdows <dwiddows@liveperson.com>
2021-05-02 14:48:36 -0700

    Lat lon lines and labels

--- map_sea.py
+++ map_sea.py
@@ -9,6 +9,7 @@ import shapefile
 
 BORDER = 20
 PIXELS_PER_DEGREE = 20
+FONT_TYPE = "Times.ttc"
 ALL_COUNTRY_RECORDS = shapefile.Reader("./data/ne_50m_admin_0_countries.shp")
 
 """ These names match with those in ALL_COUNTRY_RECORDS. 
@@ -44,15 +45,15 @@ for tmp_shape in ALL_COUNTRY_RECORDS.shapeRecords():
         polygons.append(tmp_shape.shape.points)
 
 all_points = [point for polygon in polygons for point in polygon]
-MAXLAT = max([x[0] for x in all_points])
-MINLAT = min([x[0] for x in all_points])
-MAXLON = max([x[1] for x in all_points])
-MINLON = min([x[1] for x in all_points])
+MAXLON = max([x[0] for x in all_points])
+MINLON = min([x[0] for x in all_points])
+MAXLAT = max([x[1] for x in all_points])
+MINLAT = min([x[1] for x in all_points])
 
 
 def point_to_coords(lat_lon_point):
-    return (BORDER + PIXELS_PER_DEGREE * (lat_lon_point[0] - MINLAT),
-            BORDER + PIXELS_PER_DEGREE * (MAXLON - lat_lon_point[1]))
+    return (int(BORDER + PIXELS_PER_DEGREE * (lat_lon_point[0] - MINLON)),
+            int(BORDER + PIXELS_PER_DEGREE * (MAXLAT - lat_lon_point[1])))
 
 
 def record_to_coords(record):
@@ -75,11 +76,10 @@ def draw_countries(img_draw, shape_records, fill="#dddddd"):
 
 
 def draw_name(img_draw, record):
-    font_type = "Times.ttc"
     bbox = record.shape.bbox
     size = bbox[2] - bbox[0] + bbox[3] - bbox[1]
     font_size = max(math.ceil(2 + math.pow(size, 0.3) * 10), 16)
-    font = ImageFont.truetype(font_type, font_size)
+    font = ImageFont.truetype(FONT_TYPE, font_size)
     name = record.record.NAME_EN
     w, h = font.getsize(name)
     center = point_to_coords([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2])
@@ -90,8 +90,8 @@ def draw_name(img_draw, record):
 
 
 def get_base_image():
-    size = [BORDER * 2 + int(PIXELS_PER_DEGREE * (MAXLAT - MINLAT)),
-            BORDER * 2 + int(PIXELS_PER_DEGREE * (MAXLON - MINLON))]
+    size = [BORDER * 2 + int(PIXELS_PER_DEGREE * (MAXLON - MINLON)),
+            BORDER * 2 + int(PIXELS_PER_DEGREE * (MAXLAT - MINLAT))]
     img = Image.new("RGB", size, "#f9f9f9")
     img_draw = ImageDraw.Draw(img)
 
@@ -109,8 +109,42 @@ def countries_and_names():
     img.save("sea_countries.png")
 
 
+def lat_lon_lines():
+    """Draws lines and labels including rotation.
+
+    Actually quite a hassle to get working, might be easier to annotate manually in Mac Preview or similar."""
+    img, img_draw = get_base_image()
+    dash_length = 5
+    # Equator and tropic of cancer - horizontal
+    for start, end in [((MINLON - 5, 0), (MAXLON + 5, 0)),
+                       ((MINLON - 5, 23.5), (MAXLON + 5, 23.5))]:
+        plot_start, plot_end = point_to_coords(start), point_to_coords(end)
+        while plot_start[0] < plot_end[0]:
+            img_draw.line((plot_start, (plot_start[0] + dash_length, plot_start[1])), fill="#333333")
+            plot_start = (plot_start[0] + 2*dash_length, plot_start[1])
+    # 141st meridian - vertical
+    plot_start, plot_end = point_to_coords((141, MAXLAT + 5)), point_to_coords((141, MINLAT - 5))
+    while plot_start[1] < plot_end[1]:
+        img_draw.line((plot_start, (plot_start[0], plot_start[1] + dash_length)), fill="#333333")
+        plot_start = (plot_start[0], plot_start[1] + 2 * dash_length)
+
+    font = ImageFont.truetype(FONT_TYPE, 24)
+    img_draw.text(point_to_coords((133, 1.5)), "Equator", font=font, fill="#111111")
+    img_draw.text(point_to_coords((121.8, 22.5)), "Tropic of Cancer (23.5° North)", font=font, fill="#111111")
+
+    tmp_txt = Image.new("RGBA", (400, 50))
+    tmp_draw = ImageDraw.Draw(tmp_txt)
+    tmp_draw.text((0, 0), "141st Meridian (East of Greenwich)", font=font, fill="#111111")
+    tmp_window = tmp_txt.rotate(270, expand=1)
+    img.paste(tmp_window, point_to_coords((138, 20)), tmp_window)
+
+    img = ImageOps.expand(img, border=3)
+    img.show()
+    img.save("sea_lat_lon.png")
+
+
 def main():
-    countries_and_names()
+    lat_lon_lines()
 
 
 if __name__ == '__main__':
